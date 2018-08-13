10be28d4c074867b4a86e35994c9f9e594e767a6
PeterKhoa <peterkhoa@gmail.com>
2018-08-13 13:40:46 +0700

    Vagrant step 5: Install MySQL Server 5.7 using shell script

--- setup-as-root.sh
+++ setup-as-root.sh
@@ -6,3 +6,9 @@ apt-get install -y apache2
 
 # Install PHP
 apt-get install -y php libapache2-mod-php php-cli
+
+# Install MySQL Server
+export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password root"
+apt-get install -y mysql-server php-mysql
